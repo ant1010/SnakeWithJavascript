2ad9c44ef49d0cf6d2509f20f0c622d359843c37
ant1010 <arreola.antony@gmail.com>
2022-05-21 01:26:27 -0600

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Datastore = require('nedb');
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('../public'));
+app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
